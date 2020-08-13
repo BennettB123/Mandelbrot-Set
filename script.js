0dc12ea88b6cca63550682e12e5e92f18445f079
Bennett Ballentine <bennett.ballentine@gmail.com>
2020-08-12 21:56:06 -0400

    added support for zooming in

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// Grab canvas element and set dimensions
+// Grab canvas elements and set dimensions
 var canvas = document.getElementById('html-canvas');
 var context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// Second canvas layer is used to draw rectangles for zooming in
+var canvas2 = document.getElementById('html-canvas-top-layer');
+var context2 = canvas2.getContext('2d');
+canvas2.width = window.innerWidth;
+canvas2.height = window.innerHeight;
 
 // GLOBAL VARIABLES
 var MIN_X = -2.25;
@@ -19,8 +25,8 @@ var START_MAX_Y;
 setStartCoords(CANVAS_ASPECT_RATIO, MANDELBROT_ASPECT_RATIO);
 
 var undoStack = []; //contains previous min/max x & y values in order to undo a zoom
-var MAX_ITERATIONS = 10;
-var scaleFactor = 10;
+var MAX_ITERATIONS = 200;
+var scaleFactor = 1;
 
 var curMinX = START_MIN_X;
 var curMinY = START_MIN_Y;
@@ -31,33 +37,128 @@ var curMaxY = START_MAX_Y;
 function resized(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    canvas2.width = window.innerWidth;
+    canvas2.height = window.innerHeight;
     CANVAS_ASPECT_RATIO = canvas.width / canvas.height;
+
     setStartCoords(CANVAS_ASPECT_RATIO, MANDELBROT_ASPECT_RATIO);
     drawMandelbrotSet(START_MIN_X, START_MIN_Y, START_MAX_X, START_MAX_Y);
 }
 var resize;
-window.onresize = function(){
-  clearTimeout(resize);
-  resize = setTimeout(resized, 1);
-};
+window.addEventListener('resize', function(){
+    clearTimeout(resize);
+    resize = setTimeout(resized, 100);
+});
+
+var dragStartX;
+var dragStartY;
+var dragEndX;
+var dragEndY;
+var dragStartScreenLocX;
+var dragStartScreenLocY;
+var mouseDown = false;
+
+// Event listeners to listen for when a mouse/touch is started
+window.addEventListener('mousedown', function(event) {
+    dragStartScreenLocX = event.clientX;
+    dragStartScreenLocY = event.clientY;
+    var coords = screenLocationToCoords(event.clientX, event.clientY);
+    dragStartX = coords.x;
+    dragStartY = coords.y;
+    mouseDown = true;
+});
+window.addEventListener('touchstart', function(event) {
+    console.log(event);
+    dragStartScreenLocX = event.touches[0].clientX;
+    dragStartScreenLocY = event.touches[0].clientY;
+    var coords = screenLocationToCoords(event.touches[0].clientX, event.touches[0].clientY);
+    dragStartX = coords.x;
+    dragStartY = coords.y;
+    mouseDown = true;
+}, { passive: false });
+
+// Event listeners to listen for when a mouse/touch is moved
+// This will draw a box between the initial touch point and the current point
+window.addEventListener('mousemove', function(event) {
+    // clear top layer canvas and draw a rectangle
+    if(mouseDown){
+        context2.clearRect(0, 0, canvas2.width, canvas2.height);
+        context2.strokeStyle = 'rgba(255,255,255)';
+        context2.lineWidth = 3;
+        context2.strokeRect(dragStartScreenLocX, dragStartScreenLocY, event.clientX - dragStartScreenLocX, event.clientY - dragStartScreenLocY);
+    }
+});
+window.addEventListener('touchmove', function(event) {
+    // clear top layer canvas and draw a rectangle
+    if(mouseDown){
+        dragEndX = event.touches[0].clientX;
+        dragEndY = event.touches[0].clientY;
+
+        context2.clearRect(0, 0, canvas2.width, canvas2.height);
+        context2.strokeStyle = 'rgba(255,255,255)';
+        context2.lineWidth = 3;
+        context2.strokeRect(dragStartScreenLocX, dragStartScreenLocY, event.touches[0].clientX - dragStartScreenLocX, event.touches[0].clientY - dragStartScreenLocY);
+    }
+}, { passive: false });
+
+// Event listeners to listen for when a mouse/touch is ended
+// This will also redraw the Mandelbrot set to reflect the 
+window.addEventListener('mouseup', function(event) {
+    context2.clearRect(0, 0, canvas2.width, canvas2.height);
+    mouseDown = false;
+
+    // set new current coordinates (also check to ensure that minX < maxX etc...)
+    var coords = screenLocationToCoords(event.clientX, event.clientY);
+    curMinX = Math.min(dragStartX, coords.x);
+    curMinY = Math.min(dragStartY, coords.y);
+    curMaxX = Math.max(dragStartX, coords.x);
+    curMaxY = Math.max(dragStartY, coords.y);
+
+    // re-draw mandelbrot
+    drawMandelbrotSet(curMinX, curMinY, curMaxX, curMaxY);
+});
+window.addEventListener('touchend', function() {
+    context2.clearRect(0, 0, canvas2.width, canvas2.height);
+    mouseDown = false;
+
+    // set new current coordinates (also check to ensure that minX < maxX etc...)
+    var coords = screenLocationToCoords(dragEndX, dragEndY);
+    curMinX = Math.min(dragStartX, coords.x);
+    curMinY = Math.min(dragStartY, coords.y);
+    curMaxX = Math.max(dragStartX, coords.x);
+    curMaxY = Math.max(dragStartY, coords.y);
+
+    // re-draw mandelbrot
+    drawMandelbrotSet(curMinX, curMinY, curMaxX, curMaxY);
+}, { passive: false });
 
 // Calculates and sets the starting coordinates for the mandelbrot set
 // This function is to make the whole Mandelbrot set visible on the screen,
 // while maintaining a correct aspect ratio so it does not appear stretched.
 function setStartCoords(canvas_aspect_ratio, mandelbrot_aspect_ratio) {
-    // for wide displays (desktop)
+    // for wider displays (desktop)
     if (canvas_aspect_ratio >= mandelbrot_aspect_ratio) {
         START_MIN_X = (MIN_X * (canvas_aspect_ratio / mandelbrot_aspect_ratio));
         START_MAX_X = MAX_X * (canvas_aspect_ratio / mandelbrot_aspect_ratio);
         START_MIN_Y = MIN_Y;
         START_MAX_Y = MAX_Y;
     }
-    // for tall displays (mobile)
+    // for taller displays (mobile)
     else {
         START_MIN_X = MIN_X;
         START_MAX_X = MAX_X;
         START_MIN_Y = MIN_Y * (mandelbrot_aspect_ratio / canvas_aspect_ratio);
         START_MAX_Y = MAX_Y * (mandelbrot_aspect_ratio / canvas_aspect_ratio);
+    }
+}
+
+// Converts a pair of screen coordinates to the cooresponding coordinates of the mandelbrot set.
+// This will be used to locate where a user has decided to zoom in.
+// Return value is in the form: {x: number, y: number}
+function screenLocationToCoords(col, row) {
+    return {
+        x: scale(col, 0, canvas.width, curMinX, curMaxX),
+        y: scale(row, 0, canvas.height, curMinY, curMaxY),
     }
 }
 
@@ -103,7 +204,7 @@ function drawMandelbrotSet(minR, minC, maxR, maxC) {
         }
     }
 
-    undoStack = [[START_MIN_X, START_MAX_X, START_MIN_Y, START_MAX_Y]];
+    undoStack.push([minR, minC, maxR, maxC]);
 }
 
 drawMandelbrotSet(curMinX, curMinY, curMaxX, curMaxY);
